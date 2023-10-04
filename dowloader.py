d845caddca3ed2c3ab784c3c09614a666f14e40a
martincangaa <91851271+martincangaa@users.noreply.github.com>
2023-10-04 22:58:06 +0200

    Update dowloader.py

--- dowloader.py
+++ dowloader.py
@@ -3,31 +3,31 @@
 import pkg_resources
 import subprocess
 
-directorio = "" # Pega aqui el directorio en el que quieras que se descargue el video Ej(C:/Users/juancho/Desktop) PD: Recuerda usar "/" en vez de "\"
+directorio = "" # Paste here the directory in which you want to download the file
 
 library = "pytube"
 try:
     
     pkg_resources.get_distribution(library)
-    print(f"{library} está ya instalado")
+    print(f"{library} it is installed")
 except pkg_resources.DistributionNotFound:
     
-    print(f"{library} no esta instalado. Instalando {library}...")
+    print(f"{library} it is not installed. Installing {library}...")
     subprocess.check_call(["pip", "install", library])
-    print(f"{library} se ha instalado yuhuu!!")
+    print(f"{library} it has been installed yuhuu!!")
 
 from pytube import YouTube
 
-print("Que quieres hacer [1] descargar video / [2] descargar solo el audio:")
+print("What do you want to do [1] download video / [2] download only the audio:")
 
 start  = int(input()) 
-url = str(input("Pega el link de tu video: "))
+url = str(input("Paste the link of the video: "))
 
 def getStart():
     if(start == 1 or start == 2):
         return start
     else:
-        print("No has introducido un valor valido")
+        print("You have not introduced a valid value.")
 
 def descargar_video():
     if(getStart() == 1):  
@@ -35,12 +35,12 @@ def descargar_video():
         try:
             yt.download(directorio)
         except:
-            print("Error...")
+            print("Error downloading the video.")
     else:
         yt = YouTube(url).streams.get_audio_only()
         try:
             yt.download(directorio)
         except:
-            print("Error descargando el video...")
+            print("Error downloading the audio.")
     
 descargar_video()
